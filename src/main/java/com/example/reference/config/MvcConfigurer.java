675b4abe870c935df5fcfdb0d5d3ec5435937b35
DESKTOP-70Q55TC\a <cz2123zc@gmail.com>
2024-05-27 17:56:24 +0900

    :sparkles: STOMP 는 에러 핸들러만

---
+++ src/main/java/com/example/reference/config/MvcConfigurer.java
@@ -0,0 +1,21 @@
+//package com.example.reference.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@RequiredArgsConstructor
+//@Slf4j
+//@Configuration
+//public class MvcConfigurer implements WebMvcConfigurer {
+//
+//    // 접속 로그 인터셉터 주입
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor();
+//    }
+//
+//}
