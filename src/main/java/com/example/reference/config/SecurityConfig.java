ea95ac853826cf5a066bf866f12ec1f71b74549e
DESKTOP-70Q55TC\a <cz2123zc@gmail.com>
2024-05-27 17:18:40 +0900

    :sparkles: Security 액세스 디나이 핸들러, 엔트리 포인트 제외

--- src/main/java/com/example/reference/config/SecurityConfig.java
+++ src/main/java/com/example/reference/config/SecurityConfig.java
@@ -1,12 +1,25 @@
 package com.example.reference.config;
 
+import com.example.reference.config.jwt.CustomReTokenFilter;
+import com.example.reference.config.jwt.JwtSecurityConfig;
+import com.example.reference.config.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.filter.CorsFilter;
 
 @RequiredArgsConstructor
 @Configuration
@@ -14,10 +27,55 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CorsFilter corsFilter;
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(new CustomReTokenFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+            .authorizeHttpRequests((authorize) -> authorize
+                .requestMatchers(new AntPathRequestMatcher("/ws")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/pubsub/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/swagger-ui/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/v3/api-docs/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/swagger-ui.html")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/api/api/user/login")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/api/api/user/signup")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/api/api/user/refresh")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/api/api/user/logout")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/api/api/admin/login")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/api/api/admin/signup")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/api/api/admin/refresh")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/api/api/admin/logout")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/api/user/**")).hasAnyRole("USER", "ADMIN")
+                .requestMatchers(new AntPathRequestMatcher("/api/admin/**")).hasRole("ADMIN")
+                .anyRequest().authenticated()
+            )
+            .headers(headers -> headers.frameOptions(FrameOptionsConfig::deny))
+            .sessionManagement(sessionManagement -> sessionManagement
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            ;
+
+        return http.build();
+
+    }
+
+
 
 }
